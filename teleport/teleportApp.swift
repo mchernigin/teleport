995b0a0834ee4771acb0c3507f951d54df9e918f
Michael Chernigin <michaelchernigin@gmail.com>
2026-04-21 02:04:12 +0400

    initial commit

---
+++ teleport/teleportApp.swift
@@ -0,0 +1,17 @@
+//
+//  teleportApp.swift
+//  teleport
+//
+//  Created by Michael Chernigin on 21.04.2026.
+//
+
+import SwiftUI
+
+@main
+struct teleportApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
